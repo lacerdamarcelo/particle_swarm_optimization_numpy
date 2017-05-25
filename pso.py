c43e4165fe6299b082aa2221271dfb1060b31f5d
lacerdamarcelo <europio123@gmail.com>
2017-05-25 11:09:43 -0300

    Working on pso vanilla. For some reason, data is not updating through the iterations.

--- pso.py
+++ pso.py
@@ -8,7 +8,7 @@ class PSO:
     def __init__(self, initial_inertia_factor, final_inertia_factor,
                  initial_cognitive_coefficient, final_cognitive_coefficient,
                  initial_social_coefficient, final_social_coefficient,
-                 population_size, problem, boundaries):
+                 population_size, problem, boundaries, max_iterations):
         self.population = np.asarray([[]] * population_size)
         self.velocities = np.zeros((population_size, problem.num_dimensions))
 
@@ -35,10 +35,27 @@ class PSO:
         self.boundaries = boundaries
         self.problem = problem
 
+        self.max_iterations = max_iterations
+        self.current_iteration = 0
+
+    def init_algorithm(self):
+        self.init_population()
+        self.update_local_bests()
+
+    def run(self):
+        print(self.population)
+        for current_iteration in range(0, self.max_iterations):
+            self.update_velocity()
+            self.update_position()
+            self.update_personal_bests()
+            self.update_local_bests()
+            self.update_coefficients()
+        print(self.population)
+
     def init_array(self, arr):
         return ((self.boundaries[1] - self.boundaries[0]) *
                 np.random.random_sample((self.problem.num_dimensions,
-                                         ))) + self.boundaries[0]
+                                        ))) + self.boundaries[0]
 
     def define_local_bests(self, arr):
         max_index = np.argmax(arr)
@@ -59,11 +76,37 @@ class PSO:
         self.local_bests = split[1]
         self.local_bests_fitnesses = split[2]
 
+    def define_personal_bests(self, arr):
+        current_position = arr[:self.problem.num_dimensions]
+        current_fitness = arr[self.problem.num_dimensions]
+        personal_best_position = arr[self.problem.num_dimensions + 1:(2 * self.problem.num_dimensions) + 1]  # noqa
+        personal_best_fitness = arr[(2 * self.problem.num_dimensions) + 1:(2 * self.problem.num_dimensions) + 2]  # noqa
+        if current_fitness > personal_best_fitness:
+            return np.append(current_position, current_fitness)
+        else:
+            return np.append(personal_best_position, personal_best_fitness)
+
+    def update_personal_bests(self):
+        current_fitnesses = np.apply_along_axis(func1d=self.problem.evaluate_function,  # noqa
+                                                axis=1,
+                                                arr=self.population)
+        data = np.concatenate((self.population, current_fitnesses.reshape(self.population.shape[0], 1)), axis=1)  # noqa
+        data = np.concatenate((data, self.personal_bests), axis=1)
+        data = np.concatenate((data, self.personal_bests_fitnesses.reshape(self.population.shape[0], 1)), axis=1)  # noqa
+        new_personal_bests = np.apply_along_axis(func1d=self.define_personal_bests,  # noqa
+                                                 axis=1,
+                                                 arr=data)
+        split = np.split(new_personal_bests,
+                         indices_or_sections=[0, self.problem.num_dimensions],
+                         axis=1)
+        self.personal_bests = split[1]
+        self.personal_bests_fitnesses = split[2].reshape(self.population.shape[0])
+
     def init_population(self):
         self.population = np.apply_along_axis(func1d=self.init_array,
                                               axis=1,
                                               arr=self.population)
-        self.fitnesses = np.apply_along_axis(func1d=self.problem.evaluate_function,
+        self.fitnesses = np.apply_along_axis(func1d=self.problem.evaluate_function,  # noqa
                                              axis=1,
                                              arr=self.population)
         self.personal_bests = np.copy(self.population)
@@ -83,11 +126,13 @@ class PSO:
     def update_position(self):
         self.population += self.velocities
 
+    def update_coefficients(self):
+        self.inertia_factor += float(self.final_inertia_factor - self.initial_inertia_factor)/self.max_iterations
+        self.cognitive_coefficient += float(self.final_cognitive_coefficient - self.initial_cognitive_coefficient)/self.max_iterations
+        self.social_coefficient += float(self.final_social_coefficient - self.initial_social_coefficient)/self.max_iterations
+
 if __name__ == '__main__':
     problem = SphereFunction(2)
-    pso = PSO(0, 0, 0, 0, 0, 0, 10, problem, (-10, 10))
-    pso.init_population()
-    pso.update_local_bests()
-    pso.update_velocity()
-    pso.update_position()
-    print(pso.population)
+    pso = PSO(0, 0, 0, 0, 0, 0, 10, problem, (-10, 10), 1000)
+    pso.init_algorithm()
+    pso.run()
